a26de45a56f7c9d786bf7fe3866d3daa8f034853
Lawrence <lawrence.langen@gmail.com>
2023-01-30 11:16:46 +0100

    sysout_"Hi"

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,12 +3,19 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+    // Instanziierung class --> object / instance
+    Cat cat = new Cat();
+    System.out.println(cat);
+    cat.tellYourAddress();  
+
+    System.out.println("--------------------");
+
+    Cat cat2 = new Cat();
+    System.out.println(cat2);
+    cat2.tellYourAddress();
     }
 
   
-
-
-
 }
 
