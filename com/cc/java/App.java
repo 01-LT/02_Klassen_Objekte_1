71e9f82cbd4a33605897c89b0dbef931c3dd3023
Lawrence <lawrence.langen@gmail.com>
2023-01-30 12:04:08 +0100

    2nd_Commit

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -5,14 +5,16 @@ public class App {
     public static void main(String[] args) {
 
     // Instanziierung class --> object / instance
+    // Cat --> Referenztyp
+    // cat --> Referenzvariable
     Cat cat = new Cat();
-    System.out.println(cat);
+    System.out.println("Blick von aussen " + cat);
     cat.tellYourAddress();  
 
     System.out.println("--------------------");
 
     Cat cat2 = new Cat();
-    System.out.println(cat2);
+    System.out.println("Blick von aussen: "+  cat2);
     cat2.tellYourAddress();
     }
 
