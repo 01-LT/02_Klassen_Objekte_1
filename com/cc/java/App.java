6a163059ea9c960de509f9ddd34fa8061ae506ee
Lawrence <lawrence.langen@gmail.com>
2023-01-30 12:50:39 +0100

    3rd_Commit_shortcut_output

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,21 +3,25 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-
+    
     // Instanziierung class --> object / instance
     // Cat --> Referenztyp
     // cat --> Referenzvariable
+
     Cat cat = new Cat();
     System.out.println("Blick von aussen " + cat);
-    cat.tellYourAddress();  
+    output(cat.tellYourAddress());  
+
 
     System.out.println("--------------------");
 
     Cat cat2 = new Cat();
-    System.out.println("Blick von aussen: "+  cat2);
-    cat2.tellYourAddress();
+    output("Blick von aussen: "+  cat2);
+    output(cat2.tellYourAddress());
     }
 
-  
+  public static void output(String outputStr) {
+    System.out.println(outputStr);
+  }
 }
 
