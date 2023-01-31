92869f122ab580df5eb0467bd84c2d81485f49a6
Lawrence <lawrence.langen@gmail.com>
2023-01-31 11:56:56 +0100

    Generating Constructors

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,46 +3,38 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-    
+
+      Cat cat = new Cat("Grizabella", "white", 29);
       
-    // Instanziierung class --> object / instance
-    // Cat --> Referenztyp
-    // cat --> Referenzvariable
+      // Instanziierung class --> object / instance
+      // Cat --> Referenztyp
+      // cat --> Referenzvariabl
+        output(cat.tellYourName());
+        output(cat.tellYourFurColor());
+        output(String.valueOf(cat.tellYourAge()));  
 
-    Cat cat = new Cat();
-    
-    cat.name = "Grizabella";
-    cat.furColor = "white";
-    cat.age = 29;
+        output("Blick von aussen: " + cat);
+        output(cat.tellYourAddress());
 
-    output(cat.tellYourName());
-    output(cat.tellYourFurColor());
-    output(String.valueOf(cat.tellYourAge()));
-       
-    output("Blick von aussen: " + cat);
-    output(cat.tellYourAddress());  
+        output("-------------------------");
 
-   
-    output("--------------------");
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
 
-    Cat cat2 = new Cat();
+        output(cat2.tellYourName());
+        output(cat2.tellYourFurColor());
+        output(String.valueOf(cat2.tellYourAge())); 
 
-    cat2.name = "Alonzo";
-    cat2.furColor = "grey";
-    cat2.age = 35;
+        output("Blick von aussen: " + cat2);
+        output(cat2.tellYourAddress());
 
-    
-    output(cat2.tellYourName());
-    output(cat2.tellYourFurColor());
-    output(String.valueOf(cat2.tellYourAge()));
-    
-    output("Blick von aussen: "+  cat2);
-    output(cat2.tellYourAddress());
     }
 
-  
-  public static void output(String outputStr) {
-    System.out.println(outputStr);
-  }
+    public static void output(String outputStr) {
+        System.out.println(outputStr);
+    }
+
 }
 
+  // Instanziierung class --> object / instance
+    // Cat --> Referenztyp
+    // cat --> Referenzvariabl
