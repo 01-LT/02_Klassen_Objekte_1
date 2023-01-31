3d1b6d1b0b34b15bbb860c4bd5ce86762502e94f
Lawrence <lawrence.langen@gmail.com>
2023-01-31 11:51:35 +0100

    save

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -10,37 +10,37 @@ public class App {
     // cat --> Referenzvariable
 
     Cat cat = new Cat();
-
+    
     cat.name = "Grizabella";
     cat.furColor = "white";
     cat.age = 29;
-
 
     output(cat.tellYourName());
     output(cat.tellYourFurColor());
     output(String.valueOf(cat.tellYourAge()));
        
-    output("Blick von aussen " + cat);
+    output("Blick von aussen: " + cat);
     output(cat.tellYourAddress());  
 
-
+   
     output("--------------------");
 
     Cat cat2 = new Cat();
 
-    cat.name = "Alonzo";
-    cat.furColor = "grey";
-    cat.age = 35;
+    cat2.name = "Alonzo";
+    cat2.furColor = "grey";
+    cat2.age = 35;
 
     
-    output(cat.name);
-    output(cat.furColor);
-    output(String.valueOf(cat.age));
+    output(cat2.tellYourName());
+    output(cat2.tellYourFurColor());
+    output(String.valueOf(cat2.tellYourAge()));
     
     output("Blick von aussen: "+  cat2);
     output(cat2.tellYourAddress());
     }
 
+  
   public static void output(String outputStr) {
     System.out.println(outputStr);
   }
